66c81c1c1cd0bd189537b31e9768c08c7dd76998
Jelani Woods <jelani@firstdraft.com>
2020-06-30 15:20:32 -0500

    Starting point

---
+++ class_person.rb
@@ -0,0 +1,11 @@
+# Define a class Person with:
+
+#  - An attribute first_name
+#  - An attribute last_name
+#  - An attribute birthdate (we just assign a string to this attribute, e.g. "April 19, 1987")
+#  - An instance method, full_name, that puts first_name and last_name together
+#  - An instance method, age, that calculates the number of years between today and birthdate
+
+class Person
+  
+end
